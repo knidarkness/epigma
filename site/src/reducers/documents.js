0edf375c14407f10ecd71cb884af167cde8afa85
ninabondar <bondar_n@ucu.edu.ua>
2018-03-26 18:36:11 +0300

    created test infrastructure & tests for document reducer

--- site/src/reducers/documents.js
+++ site/src/reducers/documents.js
@@ -1,16 +1,19 @@
-import {CREATE_DOCUMENT, DELETE_DOCUMENT, RENAME_DOCUMENT, ITEMS_FETCH_DATA_SUCCESS} from "../actions/actionTypes";
+const actionTypes = require('../actions/actionTypes');
+
+//import {CREATE_DOCUMENT, DELETE_DOCUMENT, RENAME_DOCUMENT, ITEMS_FETCH_DATA_SUCCESS} from "../actions/actionTypes";
+
 
 const illustration = (state, action) => {
     switch (action.type) {
-        case CREATE_DOCUMENT:
+        case actionTypes.CREATE_DOCUMENT:
             return {
                 id: action.id,
                 name: action.name,
                 editedAt: action.editedAt
             };
-        case DELETE_DOCUMENT:
+        case actionTypes.DELETE_DOCUMENT:
             return null;
-        case RENAME_DOCUMENT:
+        case actionTypes.RENAME_DOCUMENT:
             if (state.id === action.id){
                 return {
                     id: state.id,
@@ -25,19 +28,19 @@ const illustration = (state, action) => {
 
 const documents = (state=[], action) => {
     switch (action.type){
-        case ITEMS_FETCH_DATA_SUCCESS:
+        case actionTypes.ITEMS_FETCH_DATA_SUCCESS:
             return [...action.items]
                 .sort((a, b) => b.editedAt - a.editedAt);
-        case CREATE_DOCUMENT:
+        case actionTypes.CREATE_DOCUMENT:
             return [
                 ...state,
                 illustration(undefined, action)
             ]
                 .sort((a, b) => b.editedAt - a.editedAt);
-        case DELETE_DOCUMENT:
+        case actionTypes.DELETE_DOCUMENT:
             return state
                 .filter((d) => d.id !== action.id);
-        case RENAME_DOCUMENT:
+        case actionTypes.RENAME_DOCUMENT:
             return state
                 .map(p => illustration(p, action))
                 .sort((a, b) => b.editedAt - a.editedAt);
@@ -46,4 +49,5 @@ const documents = (state=[], action) => {
     }
 };
 
-export default documents;
+module.exports = documents;
+//export default documents;
