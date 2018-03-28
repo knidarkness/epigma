be2a7d94eee530d3edba2bdda9ce86024e526efe
Taras Zelyk <taras.zelyk@gmail.com>
2018-03-28 21:48:48 +0300

    add bitcoin wallet for donations

--- site/src/components/Header/Header.js
+++ site/src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => (
         <div className='page-header__right-wrapper'>
             <h2 className='page-header__moto'>Free online vector graphics editor</h2>
             <nav className='page-navigation'>
-                <a href="#" className='page-navigation__item'>Donate</a>
+                <a href="bitcoin:17u6LMHUcwXv1c7PYwBvWw1H9kB5BY3WCL" className='page-navigation__item'>Donate</a>
                 <a href="#" className='page-navigation__item'>F_ck off</a>
             </nav>
         </div>
