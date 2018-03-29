ac3f4082ea2465d993aaba60381c82afe334a441
knidarkness <knidarkness@gmail.com>
2018-03-29 09:40:35 +0300

    fix f_ck off link

--- site/src/components/Header/Header.js
+++ site/src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => (
             <h2 className='page-header__moto'>Free online vector graphics editor</h2>
             <nav className='page-navigation'>
                 <a href="bitcoin:1J5U3ePyKxfCZy1p4uoUtQtvxLt5gEVBew" className='page-navigation__item'>Donate</a>
-                <a href="#" className='page-navigation__item'>F_ck off</a>
+                <a href="https://google.com/" className='page-navigation__item'>F_ck off</a>
             </nav>
         </div>
     </header>
