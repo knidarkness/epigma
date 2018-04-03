c9d9b29526f71ac113e9fd1a47877cb1d4bd19b1
knidarkness <knidarkness@gmail.com>
2018-04-03 14:53:43 +0300

    removed redundant code

--- site/src/utils/svg.js
+++ site/src/utils/svg.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import v4 from 'uuid/v4';
 
-const renderShape = (shape, i = 0) => {
+const renderShape = (shape) => {
     if (shape.nodes.length === 0) return;
     const shapeNodesPath = shape.nodes
         .reduce((prev, current) => prev + `${current[0]},${current[1]} `, '');
-    return (<polyline data-shape-index={shape.id} className="shape" key={v4()} points={shapeNodesPath}
-                        style={{fill: 'none', stroke: shape.color, strokeWidth: '3'}}/>);
-}
+
+    return <polyline data-shape-index={shape.id} className="shape" key={v4()} points={shapeNodesPath}
+                        style={{fill: 'none', stroke: shape.color, strokeWidth: '3'}}/>;
+};
 
 const renderAllShapes = (shapes) => shapes.map((shape, id) => renderShape(shape, id));
 
 const renderShapeNodes = (shape) => {
     return shape
-            .map((point, i) => {
-            return <circle data-node-index={i} cx={point[0]} cy={point[1]} r="5" key={v4()} stroke="black"
-                        strokeWidth="3" fill="red"/>
-    })
-}
+            .map((point, i) => <circle data-node-index={i} cx={point[0]} cy={point[1]} r="5" key={v4()} stroke="black"
+                        strokeWidth="3" fill="red"/>)
+};
 
 export const createSVG = (selectedShape, shapes) => (
     <g>
@@ -25,4 +24,4 @@ export const createSVG = (selectedShape, shapes) => (
         {renderShapeNodes(selectedShape.nodes)}
         {renderAllShapes(shapes)}
     </g>
-)
+);
