b91f0ed6bdc355ddb07bc800a2d2b81296b6ff68
knidarkness <knidarkness@gmail.com>
2018-04-03 14:58:24 +0300

    Refactor svg creation
    * refactor svg creation -- removed from canvas code into separate module
    * removed redundant code

---
+++ site/src/utils/svg.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import v4 from 'uuid/v4';
+
+const renderShape = (shape) => {
+    if (shape.nodes.length === 0) return;
+    const shapeNodesPath = shape.nodes
+        .reduce((prev, current) => prev + `${current[0]},${current[1]} `, '');
+
+    return <polyline data-shape-index={shape.id} className="shape" key={v4()} points={shapeNodesPath}
+                        style={{fill: 'none', stroke: shape.color, strokeWidth: '3'}}/>;
+};
+
+const renderAllShapes = (shapes) => shapes.map((shape, id) => renderShape(shape, id));
+
+const renderShapeNodes = (shape) => {
+    return shape
+            .map((point, i) => <circle data-node-index={i} cx={point[0]} cy={point[1]} r="5" key={v4()} stroke="black"
+                        strokeWidth="3" fill="red"/>)
+};
+
+export const createSVG = (selectedShape, shapes) => (
+    <g>
+        {renderShape(selectedShape, -1)}
+        {renderShapeNodes(selectedShape.nodes)}
+        {renderAllShapes(shapes)}
+    </g>
+);
