4af949731fdb566bd2b542826c9f2c850c286f61
Taras Zelyk <taras.zelyk@gmail.com>
2018-04-04 19:38:44 +0300

    add unit tests for selectedShape reducer

--- site/test/unit/reducers/editor/selectedShape.test.js
+++ site/test/unit/reducers/editor/selectedShape.test.js
@@ -1,18 +1,107 @@
 import assert from 'assert';
-import {setSelectedShape} from "../../../../src/actions/atomic";
+import {setSelectedShape, selectedShapeAddNode, selectedShapeDeleteNode, selectedShapeInsertNode, selectedShapeUpdateNode} from "../../../../src/actions/atomic";
 import editedShapeReducer from '../../../../src/reducers/editor/selectedShape';
 
-describe('Edited shape tests', function () {
-    it('Set edited shape', function () {
-        const editedShape = {
+describe('Selected shape tests', function () {
+    it('Set selected shape', function () {
+        const selectedShape = {
             nodes: [
-                [100, 100, 1],
-                [200, 200, 1]
+                [100, 100],
+                [200, 200]
             ],
             color: 'yellow'
         };
-        const action = setSelectedShape(editedShape.nodes, editedShape.color);
+        const action = setSelectedShape(selectedShape.nodes, selectedShape.color);
         const actual = editedShapeReducer({}, action);
-        assert.deepEqual(editedShape, actual);
+        assert.deepEqual(selectedShape, actual);
+    })
+
+    it('Add a node into a shape', function () {
+        const selectedShape = {
+            nodes: [
+                [100, 100],
+                [200, 200],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const expected = {
+            nodes: [
+                [100, 100],
+                [200, 200],
+                [300, 300],
+                [400, 400]
+            ],
+            color: 'yellow'
+        };
+        const action = selectedShapeAddNode([400, 400]);
+        const actual = editedShapeReducer(selectedShape, action);
+        assert.deepEqual(expected, actual);
+    })
+
+    it('Insert a node into a shape', function () {
+        const selectedShape = {
+            nodes: [
+                [100, 100],
+                [200, 200],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const expected = {
+            nodes: [
+                [100, 100],
+                [400, 400],
+                [200, 200],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const action = selectedShapeInsertNode(1, [400, 400]);
+        const actual = editedShapeReducer(selectedShape, action);
+        assert.deepEqual(expected, actual);
+    })
+
+    it('Update a node', function () {
+        const selectedShape = {
+            nodes: [
+                [100, 100],
+                [200, 200],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const expected = {
+            nodes: [
+                [100, 100],
+                [400, 400],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const action = selectedShapeUpdateNode(1, [400, 400]);
+        const actual = editedShapeReducer(selectedShape, action);
+        assert.deepEqual(expected, actual);
+    })
+    
+    it('Delete a node', function () {
+        const selectedShape = {
+            nodes: [
+                [100, 100],
+                [200, 200],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const expected = {
+            nodes: [
+                [100, 100],
+                [300, 300]
+            ],
+            color: 'yellow'
+        };
+        const action = selectedShapeDeleteNode(1);
+        const actual = editedShapeReducer(selectedShape, action);
+        assert.deepEqual(expected, actual);
     })
 });
